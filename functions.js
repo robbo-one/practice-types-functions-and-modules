8b581f717c2ffc999b5fc76f55106d74b785ab7b
Isaac Sedcole <44042755+Isaac-Sedcole@users.noreply.github.com>
2021-01-05 11:37:19 +1300

    competed functions

--- functions.js
+++ functions.js
@@ -15,11 +15,27 @@ function filter(arr, func) {
   return newArr
 }
 
+function find(arr,func) {
+  for(let i = 0; i < arr.length; i++) {
+    if(func(arr[i])) {return arr[i]}
+  }
+}
+
+function map(arr,func) {
+  let newArr = []
+  for (let i = 0; i < arr.length; i++) {
+    newArr[i] = func(arr[i])
+  }
+  return newArr
+}
+
 
 
 
 module.exports = {
   callsFunction: callsFunction,
   callsProperty: callsProperty,
-  filter: filter
+  filter: filter,
+  find: find,
+  map: map
 }
