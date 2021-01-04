e59b2700b96080d03c9072b48dbf3bfaa52acfa7
student <student@eda-bc-02.localdomain>
2021-01-05 11:56:17 +1300

    types

---
+++ types.js
@@ -0,0 +1,23 @@
+module.exports = {
+    getBoolean: getBoolean
+}
+ function getBoolean (){
+    var bool = true
+    return bool
+ }
+
+
+ module.exports = {
+     getFunction: getFunction
+}
+ function getFunction () {
+      return getFunction
+ }
+
+ module.exports = {
+     getNull: getNull
+ }
+function getNull () {
+    getNull = null
+    return getNull
+}
